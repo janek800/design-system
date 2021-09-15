3423a0ff167ecd0cc6ff06533322e6de0b4a1e7e
Jan Radzikowski <jan.radzikowski@bridgemaker.com>
2021-09-15 12:53:59 +0200

    Added description to stores in docs

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -13,24 +13,44 @@ export default {
 const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
+
 Primary.args = {
   primary: true,
   label: 'Button',
 };
 
+Primary.parameters = {
+  storyDescription: 'Shows primary button'
+}
+
 export const Secondary = Template.bind({});
+
 Secondary.args = {
   label: 'Button',
 };
 
+Secondary.parameters = {
+  storyDescription: 'Shows secondary button'
+}
+
 export const Large = Template.bind({});
+
 Large.args = {
   size: 'large',
   label: 'Button',
 };
 
+Large.parameters = {
+  storyDescription: 'Shows large button size'
+}
+
 export const Small = Template.bind({});
+
 Small.args = {
   size: 'small',
   label: 'Button',
 };
+
+Small.parameters = {
+  storyDescription: 'Shows small button size'
+}
