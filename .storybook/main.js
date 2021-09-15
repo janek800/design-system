b3914865ac0411b74b1c308a542d765c48ae6200
Jan Radzikowski <jan.radzikowski@bridgemaker.com>
2021-09-15 09:35:17 +0200

    Added example button component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../src/components/**/*.stories.js'
   ],
   "addons": [
     "@storybook/addon-links",
